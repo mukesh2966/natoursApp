b46ca4e7eafed591ad520f539ae6a637f48b894a
mukesh2966 <mukeshrepo2966@gmail.com>
2020-08-15 03:59:41 +0530

    reverted complete to completed + changed line_items to display_items

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -32,7 +32,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       {
         name: `${tour.name} Tour`,
         description: tour.summary,
-        images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+        images: [
+          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
+        ],
         amount: Math.ceil(tour.price * 74.89 * 100),
         currency: 'inr',
         quantity: 1,
@@ -72,7 +74,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = Math.ceil(session.line_items[0].amount / (74.89 * 100));
+  const price = Math.ceil(session.display_items[0].amount / (74.89 * 100));
   await Booking.create({
     tour,
     user,
@@ -94,7 +96,7 @@ exports.webhookCheckout = (req, res, next) => {
     return res.status(400).send(`Webhook error: ${error.message}`);
   }
 
-  if (event.type === 'checkout.session.complete') {
+  if (event.type === 'checkout.session.completed') {
     createBookingCheckout(event.data.object);
   }
 
